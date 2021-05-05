a41d8c421b6469c1c34ff6409655358928c2f041
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2021-05-05 15:21:45 -0400

    Initial implementation.

---
+++ Rock.Tests.Integration/Web/Cache/RateLimiterCacheTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rock.Bus;
+using Rock.Web.Cache;
+
+namespace Rock.Tests.Integration.Web.Cache
+{
+    [TestClass]
+    public class RateLimiterCacheTests
+    {
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+            RockMessageBus.IsRockStarted = true;
+            RockMessageBus.StartAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        [TestMethod]
+        public void Check_ShouldResetAfterPeriodExpires()
+        {
+            var result = RateLimiterCache.Check(
+                "RockTest",
+                nameof( Check_ShouldResetAfterPeriodExpires ),
+                TimeSpan.FromSeconds( 5 ),
+                1,
+                null );
+
+            Assert.IsTrue( result );
+
+            Thread.Sleep( 6000 );
+
+            result = RateLimiterCache.Check(
+                "RockTest",
+                nameof( Check_ShouldResetAfterPeriodExpires ),
+                TimeSpan.FromSeconds( 5 ),
+                1,
+                null );
+
+            Assert.IsTrue( result );
+        }
+
+        [TestMethod]
+        public void Check_ShouldReturnFalseIfCallCountExceeded()
+        {
+            var result = RateLimiterCache.Check(
+                "RockTest",
+                nameof( Check_ShouldReturnFalseIfCallCountExceeded ),
+                TimeSpan.FromSeconds( 5 ),
+                1,
+                null );
+
+            Assert.IsTrue( result );
+
+            result = RateLimiterCache.Check(
+                "RockTest",
+                nameof( Check_ShouldReturnFalseIfCallCountExceeded ),
+                TimeSpan.FromSeconds( 5 ),
+                1,
+                null );
+
+            Assert.IsFalse( result );
+        }
+
+        [TestMethod]
+        public void Check_ShouldReturnTrueIfCallCountNotExceeded()
+        {
+            var maxCount = 100;
+
+            for ( var i = 0; i < maxCount; i++ )
+            {
+                var result = RateLimiterCache.Check(
+                    "RockTest",
+                    nameof( Check_ShouldReturnTrueIfCallCountNotExceeded ),
+                    TimeSpan.FromSeconds( 5 ),
+                    maxCount,
+                    null );
+
+                Assert.IsTrue( result );
+            }
+
+            var failedResult = RateLimiterCache.Check(
+                "RockTest",
+                nameof( Check_ShouldReturnTrueIfCallCountNotExceeded ),
+                TimeSpan.FromSeconds( 5 ),
+                maxCount,
+                null );
+
+            Assert.IsFalse( failedResult );
+        }
+    }
+}
---
+++ Rock/Web/Cache/NonEntities/RateLimiterCache.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Rock.Web.Cache
+{
+    /// <summary>
+    /// This class is used to rate limit calls or block to help prevent them from being spammed.
+    /// </summary>
+    /// <seealso cref="Rock.Web.Cache.ItemCache{Rock.Web.Cache.RateLimiterCache}" />
+    [DataContract]
+    public class RateLimiterCache : ItemCache<RateLimiterCache>, IHasLifespan
+    {
+        [DataMember]
+        private int PeriodActionCount { get; set; }
+
+        [DataMember]
+        private int PeriodMaxActionCount { get; set; }
+
+        [DataMember]
+        private DateTime LastAction { get; set; }
+
+        [DataMember]
+        private TimeSpan MinTimeBetweenActions { get; set; }
+
+        [DataMember]
+        public TimeSpan? Lifespan { get; private set; }
+
+        private RateLimiterCache( TimeSpan lifespan, int maxActionsInPeriod, TimeSpan? minTimeBetweenActions = null )
+        {
+            PeriodActionCount = 0;
+            LastAction = DateTime.MinValue;
+            Lifespan = lifespan;
+
+            PeriodMaxActionCount = maxActionsInPeriod;
+
+            if ( minTimeBetweenActions == null )
+                MinTimeBetweenActions = new TimeSpan( 0 );
+            else
+                MinTimeBetweenActions = minTimeBetweenActions.Value;
+        }
+
+        public static bool Check( string actionName, string requestIdentifier, TimeSpan period, int maxActionsInPeriod, TimeSpan? minTimeBetweenActions = null )
+        {
+            var limiter = GetRateLimiter( actionName, requestIdentifier, period, maxActionsInPeriod, minTimeBetweenActions );
+            if ( limiter.CanPerformAction() )
+            {
+                limiter.PerformAction();
+                return true;
+            }
+            return false;
+        }
+
+        private void PerformAction()
+        {
+            PeriodActionCount++;
+            LastAction = DateTime.Now;
+        }
+        private bool CanPerformAction()
+        {
+            if ( PeriodActionCount >= PeriodMaxActionCount )
+                return false;
+            if ( LastAction.Add( MinTimeBetweenActions ) > DateTime.Now )
+                return false;
+            return true;
+        }
+
+        private static RateLimiterCache GetRateLimiter( string actionName, string requestIdentifier, TimeSpan period, int maxActionsInPeriod, TimeSpan? minTimeBetweenActions = null )
+        {
+            var cacheKey = GetRateLimiterCacheKey( actionName, requestIdentifier );
+            return GetOrAddExisting( cacheKey, () => InitializeNewRateLimiterCache( period, maxActionsInPeriod, minTimeBetweenActions ) );
+        }
+
+        private static RateLimiterCache InitializeNewRateLimiterCache( TimeSpan period, int maxActionsInPeriod, TimeSpan? minTimeBetweenActions = null )
+        {
+            return new RateLimiterCache( period, maxActionsInPeriod, minTimeBetweenActions );
+        }
+
+        private static string GetRateLimiterCacheKey( string actionName, string requestIdentifier )
+        {
+            return actionName + "." + requestIdentifier;
+        }
+    }
+}
