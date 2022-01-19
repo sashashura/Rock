25bf6d87545a79bed6019fed0731abb22e1fc9d4
Daniel Hazelbaker <daniel@triumph.tech>
2022-01-18 17:10:26 -0700

    - Update comments.

--- RockWeb/Plugins/com.rocksolidchurchdemo/Payment/demoGatewayControl.js
+++ RockWeb/Plugins/com.rocksolidchurchdemo/Payment/demoGatewayControl.js
@@ -26,30 +26,44 @@ System.register(["vue", "/Obsidian/Controls/gatewayControl"], function (exports_
                 },
 
                 setup(props, { emit }) {
-                    // hasCreditCardPaymentType.value will be true if block setting enabled CC payment type.
+                    // This calculates and provides the value from the Block Setting on
+                    // if the Credit Card Payment Type is enabled. You do not need to modify
+                    // any of this code. You can access the value like:
+                    //
+                    // if (hasCreditCardPaymentType.value) { /* do stuff */ }
                     const hasCreditCardPaymentType = vue_1.computed(() => {
                         var _a, _b;
                         return (_b = (_a = props.settings.enabledPaymentTypes) === null || _a === void 0 ? void 0 : _a.includes(0)) !== null && _b !== void 0 ? _b : false;
                     });
 
-                    // hasBankAccountPaymentType.value will be true if block setting enabled ACH payment type.
+                    // This calculates and provides the value from the Block Setting on
+                    // if the ACH Payment Type is enabled. You do not need to modify
+                    // any of this code. You can access the value like:
+                    //
+                    // if (hasBankAccountPaymentType.value) { /* do stuff */ }
                     const hasBankAccountPaymentType = vue_1.computed(() => {
                         var _a, _b;
                         return (_b = (_a = props.settings.enabledPaymentTypes) === null || _a === void 0 ? void 0 : _a.includes(1)) !== null && _b !== void 0 ? _b : false;
                     });
 
                     // hasPaid.value will be true when the checkbox is checked.
+                    // This is not needed but is provided as an example of how to
+                    // tie something in the UI to a backend value.
                     const hasPaid = vue_1.ref(false);
 
                     gatewayControl_1.onSubmitPayment(() => {
                         // This method is called when the page Submit button is clicked.
+                        // You can do any logic you need to start the payment here.
 
                         // Timeout is just to simulate an async call to perform actual payment.
                         setTimeout(() => {
                             if (hasPaid.value) {
+                                // Pretend that the payment was charged.
                                 handleTokenResponse("raw-token-value");
                             }
                             else {
+                                // Pretend the payment had some kind of error, treat it as
+                                // a validation error since that is likely the problem.
                                 handleValidationErrors({ "Payment Information": "is not valid" });
                             }
                         }, 500);
