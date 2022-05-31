2c1046c0e8267fc13c14041731728c1163c7d1d4
Daniel Hazelbaker <daniel@triumph.tech>
2022-05-31 08:11:12 -0700

    - Attempt to update vue build process.

---
+++ Rock.JavaScript.Obsidian/Framework/Controls/aTest.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>{{ myValue }}</div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        name: "aTest",
+
+        setup() {
+            return {
+                myValue: "hello"
+            };
+        }
+    });
+</script>
