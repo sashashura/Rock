169a00ae17f4403b90b2d0e0300c7838e095f876
Braden Cohen <braden.cohen@Yahoo.com>
2022-06-17 08:15:49 -0700

    - WIP on Deep Linking.

--- Rock/Web/HttpModules/DeepLinks.cs
+++ Rock/Web/HttpModules/DeepLinks.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
@@ -7,13 +23,15 @@ using System.Web;
 
 using Newtonsoft.Json;
 
+using Rock.Common.Mobile;
 using Rock.Data;
 using Rock.Mobile;
 using Rock.Model;
+
 namespace Rock.Web.HttpModules
 {
     /// <summary>
-    /// 
+    /// An HTTP module used to handle incoming Deep Links. 
     /// </summary>
     /// <seealso cref="Rock.Web.HttpModules.HttpModuleComponent" />
     [Description( "A HTTP Module that handles deep link requests for mobile applications." )]
@@ -39,7 +57,6 @@ namespace Rock.Web.HttpModules
             context.BeginRequest += ( new EventHandler( Application_BeginRequest ) );
         }
 
-
         /// <summary>
         /// Handles the BeginRequest event of the Application control.
         /// </summary>
@@ -55,11 +72,203 @@ namespace Rock.Web.HttpModules
             string path = HttpContext.Current.Request.Url.AbsolutePath;
 
             // Early outs in case the requested link isn't in regards to deep linking.
-            if ( !path.StartsWith( "/.well-known" ) )
+            if ( path.StartsWith( "/.well-known/" ) )
+            {
+                ProcessWellKnownRequest( context, path );
+                return;
+            }
+
+            var segments = path.SplitDelimitedValues( "/" ).ToList();
+            segments.RemoveAt( 0 );
+
+            // TODO: Cache the deep links. These are dummy deep links, but we need to fetch all the mobile sites that have deep linking enabled \
+            // and if the path prefix is the same as the beginning segment of the URL.
+
+            DeepLinkRoute matchedRoute = null;
+
+            var dynamicParameters = new Dictionary<string, string>();
+            using ( var rockContext = new RockContext() )
+            {
+                var siteService = new SiteService( rockContext );
+                var sites = siteService.Queryable().AsEnumerable().Where( x =>
+                x.AdditionalSettings?.FromJsonOrNull<AdditionalSiteSettings>().IsDeepLinkingEnabled == true
+                );
+
+                foreach ( var site in sites )
+                {
+                    if ( path.StartsWith( $"/{site.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>().DeepLinkPathPrefix}/" ) )
+                    {
+                        var (route, dynamicParams) = FindRouteWithParams( site, path );
+                        matchedRoute = route;
+
+                        if ( dynamicParams != null )
+                        {
+                            dynamicParameters = dynamicParams;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            if ( matchedRoute == null )
             {
                 return;
             }
 
+            string query = HttpContext.Current.Request.Url.Query;
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            if(query.IsNotNullOrWhiteSpace())
+            {
+                query = query.Remove( 0, 1 );
+                parameters = QuerystringToParms( query );
+            }
+
+            if ( dynamicParameters.Count() > 0 )
+            {
+                try
+                {
+                    dynamicParameters.ToList().ForEach( q => parameters.Add( q.Key, q.Value ) );
+                }
+                catch ( ArgumentException ex )
+                {
+                    System.Diagnostics.Debug.WriteLine( "Please make sure you do not have duplicate query parameters." );
+                    System.Diagnostics.Debug.WriteLine( $"{ex.InnerException?.ToStringSafe() ?? string.Empty}" );
+                    throw;
+                }
+            }
+
+            // If the route uses a Url as a fallback, we will redirect them as such.
+            if ( matchedRoute.UsesUrlAsFallback )
+            {
+                HttpContext.Current.Response.StatusCode = 303;
+                HttpContext.Current.Response.Redirect( matchedRoute.WebFallbackPageUrl );
+                HttpContext.Current.Response.End();
+            }
+            // The route falls back to a page, so let's build the route to that page 
+            else
+            {
+                using ( var rockContext = new RockContext() )
+                {
+                    var pageService = new PageService( rockContext );
+
+                    var pageSequence = pageService.Queryable().Where( x => x.Guid == matchedRoute.WebFallbackPageGuid );
+                    var page = pageSequence.Any() ? pageSequence.First() : null;
+
+                    if ( page == null )
+                    {
+                        return;
+                    }
+
+                    var pageReference = new PageReference( page.Id, page.PageRoutes.First().Id, parameters );
+
+                    var routeUrl = pageReference.BuildUrl();
+
+                    HttpContext.Current.Response.StatusCode = 301;
+                    HttpContext.Current.Response.Redirect( $"{routeUrl}" );
+                    HttpContext.Current.Response.End();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Takes a query string Var1=ValueA&Var2=ValueB&Var3=ValueC and returns a Dictionary of parameters.
+        /// </summary>
+        /// <param name="str">The querystring.</param>
+        /// <returns></returns>
+        private static Dictionary<string, string> QuerystringToParms( string str )
+        {
+            var parms = new Dictionary<string, string>( StringComparer.OrdinalIgnoreCase );
+
+            if ( str.IsNullOrWhiteSpace() )
+            {
+                return parms;
+            }
+
+            var queryStringParms = str.Split( new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries );
+
+            foreach ( var keyValue in queryStringParms )
+            {
+                string[] splitKeyValue = keyValue.Split( new char[] { '=' } );
+
+                {
+                    string unencodedKey = Uri.UnescapeDataString( splitKeyValue[0] ).Trim();
+                    string unencodedValue = Uri.UnescapeDataString( splitKeyValue[1] ).Trim();
+                    parms.AddOrReplace( unencodedKey, unencodedValue );
+                }
+            }
+
+            return parms;
+        }
+
+        /// <summary>
+        /// Finds the route with parameters.
+        /// </summary>
+        /// <param name="site">The site to cross reference the routes from.</param>
+        /// <param name="path">The path.</param>
+        /// <returns>&lt;DeepLinkRoute, Dictionary&lt;string, string&gt;&gt;.</returns>
+        public (DeepLinkRoute route, Dictionary<string, string> dynamicParams) FindRouteWithParams( Site site, string path )
+        {
+            var dynamicParameters = new Dictionary<string, string>();
+            var routes = site.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>().DeepLinkRoutes;
+
+            for ( int routeIndex = 0; routeIndex < routes.Count(); routeIndex++ )
+            {
+                var route = routes[routeIndex];
+                var absoluteRoute = route.Route;
+
+                var routeSegments = absoluteRoute.Split( '/' ).ToList();
+                var pathSegments = path.Split( '/' ).ToList();
+
+                pathSegments.RemoveAt( 0 );
+                pathSegments.RemoveAt( 0 );
+
+                if( pathSegments.Count() != routeSegments.Count() )
+                {
+                    continue;
+                }
+
+                for ( int routeSegmentIndex = 0; routeSegmentIndex <= routeSegments.Count(); routeSegmentIndex++ )
+                {
+                    var pathSegmentIndex = routeSegmentIndex;
+
+                    if ( routeSegmentIndex == routeSegments.Count() )
+                    {
+                        return (route, dynamicParameters);
+                    }
+
+                    // The segments we are comparing. (requested path e.g. '/u/christmas/32/notes' to route path e.g. '/u/christmas/{christmasId}/notes'
+                    var routeSegment = routeSegments[routeSegmentIndex];
+                    var pathSegment = pathSegments[pathSegmentIndex];
+
+                    // If this route is a dynamic segment, we need to take those values and put them into the parameters dictionary that will go to the page.
+                    if ( routeSegment.StartsWith( "{" ) && routeSegment.EndsWith( "}" ) )
+                    {
+                        var key = routeSegment.Trim( new char[] { '{', '}' } );
+                        dynamicParameters.Add( key, pathSegment );
+                        continue;
+                    }
+
+                    // The route is not a match. On to the next route.
+                    if ( routeSegment != pathSegment )
+                    {
+                        dynamicParameters.Clear();
+                        break;
+                    }
+                }
+            }
+
+            return (null, null);
+        }
+
+        /// <summary>
+        /// Processes a .well-known folder request to see if we are requesting either the AASA (iOS) file or the assetlinks.json (Android) file.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="path">The path.</param>
+        private void ProcessWellKnownRequest( HttpContext context, string path )
+        {
             var shouldGetAssetLinks = path.Contains( "assetlinks.json" );
             var shouldGetAASA = path.Contains( "apple-app-site-association" );
 
@@ -87,12 +296,12 @@ namespace Rock.Web.HttpModules
         {
             // Fetching all of our mobile sites.
             var mobileSites = GetDeepLinkSites();
-            
+
 
             // In this area, we are mostly working on constructing our data in a way that easily converts into the
             // required format of the AASA file. See: https://gist.github.com/mat/e35393e9dfd9d7fb0972
-            var appLinks = new AASAResponsePOCO();
-            var detailsList = new List<AASADeepLinkDetailsPOCO>();
+            var appLinks = new AASAResponse();
+            var detailsList = new List<AASADeepLinkDetails>();
 
             // We're going to loop through each site and do a couple of things.
             // 1. Construct the AppId and Path for each application, if unclear please refer to link above.
@@ -101,7 +310,7 @@ namespace Rock.Web.HttpModules
             {
                 var additionalSettings = site.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>();
 
-                var appDetails = new AASADeepLinkDetailsPOCO
+                var appDetails = new AASADeepLinkDetails
                 {
                     AppId = $"{additionalSettings.TeamIdentifier}.{additionalSettings.BundleIdentifier}",
                     Paths = new List<string> { $"/{additionalSettings.DeepLinkPathPrefix}/*" }
@@ -150,7 +359,7 @@ namespace Rock.Web.HttpModules
                     continue;
                 }
 
-                var appDetails = new AssetLinksTargetDetailsPOCO
+                var appDetails = new AssetLinksTargetDetails
                 {
                     PackageName = additionalSettings.PackageName,
                     CertificateFingerprint = additionalSettings.CertificateFingerprint
@@ -189,7 +398,7 @@ namespace Rock.Web.HttpModules
         /// <summary>
         /// POCO for the entire AASA response information.
         /// </summary>
-        private class AASAResponsePOCO
+        private class AASAResponse
         {
             /// <summary>
             /// Gets or sets the apps identifier, in our use case, we leave this list empty. 
@@ -207,13 +416,13 @@ namespace Rock.Web.HttpModules
             /// The deep link details list.
             /// </value>
             [JsonProperty( "details" )]
-            public List<AASADeepLinkDetailsPOCO> DetailsList { get; set; }
+            public List<AASADeepLinkDetails> DetailsList { get; set; }
         }
 
         /// <summary>
         /// POCO for the deep link details information. 
         /// </summary>
-        private class AASADeepLinkDetailsPOCO
+        private class AASADeepLinkDetails
         {
             /// <summary>
             /// Gets or sets the application identifier.
@@ -237,7 +446,7 @@ namespace Rock.Web.HttpModules
         /// <summary>
         /// POCO for the deep link details information.
         /// </summary>
-        private class AssetLinksTargetDetailsPOCO
+        private class AssetLinksTargetDetails
         {
             /// <summary>
             /// Gets or sets the namespace.
--- RockWeb/Blocks/Mobile/MobileDeepLinkDetail.ascx.cs
+++ RockWeb/Blocks/Mobile/MobileDeepLinkDetail.ascx.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -20,10 +36,16 @@ namespace RockWeb.Blocks.Mobile
     [Description( "Edits and configures the settings of mobile deep-link routes." )]
     public partial class MobileDeepLinkDetail : RockBlock
     {
+        #region Fields
+
         /// <summary>
         /// The state if we are modifying an existing deep link route.
         /// </summary>
         private bool _isModifying;
+
+        #endregion
+
+        #region Base Method Overrides
 
         ///<inheritdoc />
         protected override void OnLoad( EventArgs e )
@@ -48,119 +70,9 @@ namespace RockWeb.Blocks.Mobile
             base.OnLoad( e );
         }
 
-        #region Keys
-
         #endregion
 
         #region Methods
-
-        #endregion
-
-        #region Event Handlers
-
-        /// <summary>
-        /// Handles the Click event of the lbSave control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void lbSave_Click( object sender, EventArgs e )
-        {
-            using ( var context = new RockContext() )
-            {
-                var pageService = new PageService( context );
-                var siteService = new SiteService( context );
-
-                // Get the site settings for this specific site.
-                var site = siteService.Get( PageParameter( "SiteId" ) );
-                var additionalSettings = site.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>();
-
-                // Generate the guid for our route, and get the guid for the mobile page corresponding to it.
-                var mobilePageGuid = pageService.GetGuid( ppMobilePage.SelectedValue.AsInteger() );
-                Guid guid = Guid.NewGuid();
-
-                // If we are modifying, change our guid var to match the one we are modifying, then delete it from our route list.
-                if ( _isModifying )
-                {
-                    // We actually checked whether this is null in our ConfigureContent(), so we know it exists.
-                    guid = PageParameter( "DeepLinkRouteGuid" ).AsGuid();
-                    var refRoute = additionalSettings.DeepLinkRoutes.Where( r => r.Guid == guid ).First();
-                    additionalSettings.DeepLinkRoutes.Remove( refRoute );
-                }
-
-                var routeAlreadyExists = additionalSettings.DeepLinkRoutes.Any( r => r.Route == tbRoute.Text );
-                if ( routeAlreadyExists )
-                {
-                    // TODO: Need to tighten up this check to include potential query parameters
-                    nbError.Text = $"The route: '{tbRoute.Text}' already exists. Please choose a non-conflicting route.";
-                    return;
-                }
-
-                var webFallbackPageGuid = pageService.GetGuid( ppFallbackPage.SelectedValue.AsInteger() );
-
-                var usesUrlAsFallback = tglFallbackType.Checked;
-
-                // Clear the selection of the fallback page picker. 
-                if ( usesUrlAsFallback )
-                {
-                    webFallbackPageGuid = null;
-                }
-                // Clear the selection of the fallback URL textbox.
-                else
-                {
-                    tbFallbackUrl.Text = string.Empty;
-                }
-
-                var route = new DeepLinkRoute
-                {
-                    Guid = guid,
-                    Route = tbRoute.Text,
-                    UsesUrlAsFallback = tglFallbackType.Checked,
-                    MobilePageGuid = mobilePageGuid.Value,
-                    WebFallbackPageGuid = webFallbackPageGuid,
-                    WebFallbackPageUrl = tbFallbackUrl.Text
-                };
-
-                // Add the route and save the settings.
-                additionalSettings.DeepLinkRoutes.Add( route );
-                site.AdditionalSettings = additionalSettings.ToJson();
-                context.SaveChanges();
-
-                NavigateToParentPage( new Dictionary<string, string>
-                {
-                    { "SiteId", site.Id.ToString() },
-                    { "Tab", "Deep Links" }
-                } );
-            }
-        }
-
-        /// <summary>
-        /// Handles the Click event of the lbCancel control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void lbCancel_Click( object sender, EventArgs e )
-        {
-            NavigateToParentPage( new Dictionary<string, string>
-                {
-                    { "SiteId", PageParameter( "SiteId" ) },
-                    { "Tab", "Deep Links" }
-                } );
-        }
-
-        /// <summary>
-        /// Handles the CheckedChanged event of the tglFallbackType control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void tglFallbackType_CheckedChanged( object sender, EventArgs e )
-        {
-            ToggleFallbackContent();
-        }
-
-
-        #endregion
-
-
 
         /// <summary>
         /// Configures the content.
@@ -234,5 +146,119 @@ namespace RockWeb.Blocks.Mobile
             tbFallbackUrl.Visible = !fallbackTypeIsPage;
             tbFallbackUrl.Required = !fallbackTypeIsPage;
         }
+
+        /// <summary>
+        /// Saves the deep link.
+        /// </summary>
+        private void SaveDeepLink()
+        {
+            using ( var context = new RockContext() )
+            {
+                var pageService = new PageService( context );
+                var siteService = new SiteService( context );
+
+                // Get the site settings for this specific site.
+                var site = siteService.Get( PageParameter( "SiteId" ) );
+                var additionalSettings = site.AdditionalSettings.FromJsonOrNull<AdditionalSiteSettings>();
+
+                // Generate the guid for our route, and get the guid for the mobile page corresponding to it.
+                var mobilePageGuid = pageService.GetGuid( ppMobilePage.SelectedValue.AsInteger() );
+                Guid guid = Guid.NewGuid();
+
+                // If we are modifying, change our guid var to match the one we are modifying, then delete it from our route list.
+                if ( _isModifying )
+                {
+                    // We actually checked whether this is null in our ConfigureContent(), so we know it exists.
+                    guid = PageParameter( "DeepLinkRouteGuid" ).AsGuid();
+                    var refRoute = additionalSettings.DeepLinkRoutes.Where( r => r.Guid == guid ).First();
+                    additionalSettings.DeepLinkRoutes.Remove( refRoute );
+                }
+
+                var routeAlreadyExists = additionalSettings.DeepLinkRoutes.Any( r => r.Route == tbRoute.Text );
+                if ( routeAlreadyExists )
+                {
+                    // TODO: Need to tighten up this check to include potential query parameters
+                    nbError.Text = $"The route: '{tbRoute.Text}' already exists. Please choose a non-conflicting route.";
+                    return;
+                }
+
+                var webFallbackPageGuid = pageService.GetGuid( ppFallbackPage.SelectedValue.AsInteger() );
+
+                var usesUrlAsFallback = tglFallbackType.Checked;
+
+                // Clear the selection of the fallback page picker. 
+                if ( usesUrlAsFallback )
+                {
+                    webFallbackPageGuid = null;
+                }
+                // Clear the selection of the fallback URL textbox.
+                else
+                {
+                    tbFallbackUrl.Text = string.Empty;
+                }
+
+                var route = new DeepLinkRoute
+                {
+                    Guid = guid,
+                    Route = tbRoute.Text,
+                    UsesUrlAsFallback = tglFallbackType.Checked,
+                    MobilePageGuid = mobilePageGuid.Value,
+                    WebFallbackPageGuid = webFallbackPageGuid,
+                    WebFallbackPageUrl = tbFallbackUrl.Text
+                };
+
+                // Add the route and save the settings.
+                additionalSettings.DeepLinkRoutes.Add( route );
+                site.AdditionalSettings = additionalSettings.ToJson();
+                context.SaveChanges();
+
+                NavigateToParentPage( new Dictionary<string, string>
+                {
+                    { "SiteId", site.Id.ToString() },
+                    { "Tab", "Deep Links" }
+                } );
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handles the Click event of the lbSave control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbSave_Click( object sender, EventArgs e )
+        {
+            SaveDeepLink();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbCancel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbCancel_Click( object sender, EventArgs e )
+        {
+            NavigateToParentPage( new Dictionary<string, string>
+                {
+                    { "SiteId", PageParameter( "SiteId" ) },
+                    { "Tab", "Deep Links" }
+                } );
+        }
+
+        /// <summary>
+        /// Handles the CheckedChanged event of the tglFallbackType control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void tglFallbackType_CheckedChanged( object sender, EventArgs e )
+        {
+            ToggleFallbackContent();
+        }
+
+        #endregion
+
     }
 }
