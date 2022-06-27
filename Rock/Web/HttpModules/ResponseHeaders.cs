617fb1f28b92665d21eb3d98a86c16c5e1fcf653
Braden Cohen <braden.cohen@Yahoo.com>
2022-06-27 16:20:20 -0700

    - Revert changes to ResponseHeaders

--- Rock/Web/HttpModules/ResponseHeaders.cs
+++ Rock/Web/HttpModules/ResponseHeaders.cs
@@ -1,4 +1,4 @@
-﻿// <copyright>
+// <copyright>
 // Copyright by the Spark Development Network
 //
 // Licensed under the Rock Community License (the "License");
@@ -98,6 +98,7 @@ namespace Rock.Web.HttpModules
             // request and response properties.
             HttpApplication application = ( HttpApplication ) source;
             HttpContext context = application.Context;
+            
             foreach(var header in Headers )
             {
                 context.Response.Headers.Add( header.Key, header.Value.ToString() );
