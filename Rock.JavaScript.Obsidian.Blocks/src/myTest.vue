52ec347e2d67a1dd5aba3992651215e68dc3a383
Daniel Hazelbaker <daniel@triumph.tech>
2022-07-25 13:48:47 -0700

    - Updates before moving on. It works if you reference the files directly, @Obsidian paths do not for some reason (Volar bug?).

---
+++ Rock.JavaScript.Obsidian.Blocks/src/myTest.vue
@@ -0,0 +1,22 @@
+<template>
+    <Alert alertType="test" />
+    <ATest myValue="test" />
+    <ViewPanel options="23" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Alert from "@Obsidian/Controls/alert";
+import ATest from "../../Rock.JavaScript.Obsidian/Framework/Controls/aTest.vue";
+import ViewPanel from "./Core/CampusDetail/viewPanel";
+
+export default defineComponent({
+    name: "myTest",
+
+    components: { ATest, Alert, ViewPanel },
+
+    setup() {
+
+    }
+})
+</script>
--- Rock.JavaScript.Obsidian/Framework/Controls/aTest.vue
+++ Rock.JavaScript.Obsidian/Framework/Controls/aTest.vue
@@ -3,10 +3,18 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from "vue";
+    import { defineComponent, PropType } from "vue";
 
     export default defineComponent({
         name: "aTest",
+
+        props: {
+            /** This is a comment. */
+            myValue: {
+                type: String as PropType<string>,
+                required: false
+            }
+        },
 
         setup() {
             return {
