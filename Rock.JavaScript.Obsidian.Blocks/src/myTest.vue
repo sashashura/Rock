5030bb2d39233ec2f47ec53901b45f909363f971
Daniel Hazelbaker <daniel@triumph.tech>
2022-08-11 15:00:35 -0700

    - Update sample vue file to show errors.

--- Rock.JavaScript.Obsidian.Blocks/src/myTest.vue
+++ Rock.JavaScript.Obsidian.Blocks/src/myTest.vue
@@ -1,14 +1,14 @@
 <template>
     <Alert alertType="test" />
-    <ATest myValue="test" />
-    <ViewPanel options="23" />
+    <ATest :myValue="intValue" />
+    <ViewPanel :options="intValue" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Alert from "@Obsidian/Controls/alert";
-import ATest from "../../Rock.JavaScript.Obsidian/Framework/Controls/aTest.vue";
-import ViewPanel from "./Core/CampusDetail/viewPanel";
+import ATest from "@Obsidian/Controls/aTest.vue";
+import ViewPanel from "./Core/CampusDetail/viewPanel.partial";
 
 export default defineComponent({
     name: "myTest",
@@ -16,7 +16,12 @@ export default defineComponent({
     components: { ATest, Alert, ViewPanel },
 
     setup() {
+        /** This is a comment on a variable. */
+        const intValue = ref(23);
 
+        return {
+            intValue
+        };
     }
 })
 </script>
